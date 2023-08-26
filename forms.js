d19cc747bedcc0969a43e8a814d4a128a1ce071a
Alexander Zic <zic.alexander@gmail.com>
2023-08-26 15:54:56 -0600

    order#

--- forms.js
+++ forms.js
@@ -10,10 +10,41 @@ let currentOrder = 1; // will increment this by 1 each time we add a new order
 
 function displayOrder(event){
   //by defualt, the submet event on a form refreshes the page
-  event.preventDefualt(); // this method stops the default behavior of the event
+  event.preventDefault(); // this method stops the default behavior of the event
   console.log(event);
   console.log(event.target); //this is the element bring targeted by the event which is happening (form element)
-  console.log(form);
+  console.log(form); //the form element
+  console.log(form.orderName); // an entire input field
+  console.log(form.orderName.value);//the name input by the user
+
+  console.log(form.toppings.value);//the toppings input by the user
+
+  console.log(form.size.value); //the value from the checked radio button
+
+  /*
+  - make a variable or variables for the information from the user
+  - append some elements to the page (tr[1], td[4])
+  */
+ const row = document.createElement('tr');
+ const name = document.createElement('td');
+ const orderNumber = document.createElement('td');
+ const toppings = document.createElement('td');
+ const size = document.createElement('td');
+
+ table.appendChild(row);
+
+// when appending children, you need to be careful of the order they append. This will affect how they are displayed
+ row.appendChild(orderNumber);
+ row.appendChild(name);
+ row.appendChild(size);
+ row.appendChild(toppings);
+
+ name.innerText = form.orderName.value;
+ size.innerText = form.size.value;
+ toppings.innerText = form.toppings.value;
+ // display our current order number, then increment by 1
+ orderNumber.innerText = currentOrder;
+ currentOrder++;
 }
 
 // listen for when the form is submitted
